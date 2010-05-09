c5402c5170a34364cbbf324ce41c6e739c805b43
Craig Moore <craigmoore@flashygraphics.co.uk>
2010-05-09 19:38:01 +0100

    Updated README and removed references to signup from json.

--- javascript/json/right.js
+++ javascript/json/right.js
@@ -1,1 +1,1 @@
-["javascript\/json\/center.js","javascript\/json\/center.js","javascript\/json\/center.js","javascript\/json\/center.js","images\/green.png","http:\/\/www.flashygraphics.co.uk\/","Original source of the simple image network","http:\/\/www.flashygraphics.co.uk\/simpleimagenetwork\/signup"]
+["javascript\/json\/center.js","javascript\/json\/center.js","javascript\/json\/center.js","javascript\/json\/center.js","images\/green.png","http:\/\/www.flashygraphics.co.uk\/","Original source of the simple image network"]
--- javascript/json/up.js
+++ javascript/json/up.js
@@ -1,1 +1,1 @@
-["javascript\/json\/center.js","javascript\/json\/center.js","javascript\/json\/center.js","javascript\/json\/center.js","images\/cyan.png","http:\/\/www.flashygraphics.co.uk\/simpleimagenetwork\/","Original source of the simple image network","http:\/\/www.flashygraphics.co.uk\/simpleimagenetwork\/signup"]
+["javascript\/json\/center.js","javascript\/json\/center.js","javascript\/json\/center.js","javascript\/json\/center.js","images\/cyan.png","http:\/\/www.flashygraphics.co.uk\/simpleimagenetwork\/","Original source of the simple image network"]
